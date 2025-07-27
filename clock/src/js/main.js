98332e579a15a2065def3fb13f2319c6146fc949
turana2012 <turana>
2025-07-27 21:30:02 +0300

    clock

--- clock/src/js/main.js
+++ clock/src/js/main.js
@@ -27,3 +27,22 @@ function myTimer() {
   const d = new Date();
   document.getElementById("demo").innerHTML = d.toLocaleTimeString();
 }
+
+
+
+
+const toggleBtn = document.getElementById('dark-mode');
+const body = document.body;
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme === 'dark') {
+  body.classList.add('dark-mode');
+}
+toggleBtn.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+  
+  if (body.classList.contains('dark-mode')) {
+    localStorage.setItem('theme', 'dark');
+  } else {
+    localStorage.setItem('theme', 'light');
+  }
+});
