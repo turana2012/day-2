7fb0fde3247e458486065658d671d020d133ff6c
turana2012 <turana>
2025-07-27 15:19:40 +0300

    clock

---
+++ clock/src/js/main.js
@@ -0,0 +1,21 @@
+ const hr = document.getElementById('hour');
+    const min = document.getElementById('minute');
+    const sec = document.getElementById('secund');
+
+    function displayTime() {
+      const date = new Date();
+      const hh = date.getHours();
+      const mm = date.getMinutes();
+      const ss = date.getSeconds();
+
+      const hRotation = 30 * hh + mm / 2;
+      const mRotation = 6 * mm;
+      const sRotation = 6 * ss;
+
+    hr.style.transform = `translate(-50%, -100%) rotate(${hRotation}deg)`;
+min.style.transform = `translate(-50%, -100%) rotate(${mRotation}deg)`;
+sec.style.transform = `translate(-50%, -100%) rotate(${sRotation}deg)`;
+    }
+
+    setInterval(displayTime, 1000);
+    displayTime();
